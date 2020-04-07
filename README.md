37a76f70b14b17606b4bf802a7d0042bb127de57
punitkulli <sarupunit@gmail.com>
2020-04-07 09:25:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# adb-forarray-del
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/adb-forarray-del)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'hello',
+  templateUrl: 'hello.component.html',
+  styleUrls: ['hello.component.css']
+})
+export class HelloComponent implements OnInit {
+processCheckListForm: FormGroup;
+
+constructor(private fb: FormBuilder){}
+
+ngOnInit(){
+ const frontPageWaArray = new FormArray([]);
+    const productInformationArray = new FormArray([]);
+    const summarySheetDocsArray = new FormArray([]);
+    const remarksArray = new FormArray([]);
+
+    this.processCheckListForm = this.fb.group({
+      itemRows: frontPageWaArray /* this.fb.array([this.initItemRows()]) */,
+      itemRows1: productInformationArray,
+      itemRows2: summarySheetDocsArray,
+      itemRows3: remarksArray
+    });
+
+}
+
+ addNewRow() {
+   (<FormArray>this.processCheckListForm.get("itemRows3")).push(
+      this.fb.group({
+        processorRemarks: [""]
+      })
+    );
+}
+
+
+}
